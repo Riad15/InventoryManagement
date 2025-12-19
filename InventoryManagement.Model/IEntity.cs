133f4dde1dead12e48fb7f6241408b73ba6712ca
Asif Riad <asifriad15@gmail.com>
2025-12-20 00:28:56 +0600

    create product model

---
+++ InventoryManagement.Model/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Model
+{
+    internal interface IEntity
+    {
+        int Id { get; set; }
+        int CreatedBy { get; set; }
+        DateTime CreatedAt { get; set; }
+        int UpdatedBy { get; set; }
+        DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ InventoryManagement.Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Model
+{
+    public class Product: IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+       
+    }
+}
